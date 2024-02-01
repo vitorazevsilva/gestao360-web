d961bb6eba5f057d1b2476a8891be14c01d64530
Vitor Silva <vitorsilva10413@gmail.com>
2024-02-01 13:13:56 +0000

    Initial Configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/',
+        destination: '/auth/sign-in',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
